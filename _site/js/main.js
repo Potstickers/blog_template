6f753e132ee25272ea1470627e9c7f11af81a745
Wellington Zhao <tuc16961@temple.edu>
2013-01-27 01:23:55 -0500

    initial push

---
+++ _site/js/main.js
@@ -0,0 +1,86 @@
+$(document).ready( function(){
+		var $content_div = $('#content'), 
+			$paging_div = $('#paging');
+		var chrome_popped = false;
+
+		if(Modernizr.history){
+			//clicking on navigation link
+			$('nav ul li').delegate('a','click',function(){
+				_link = $(this).attr('href');
+				$('li.navselected').removeClass('navselected');
+				$(this).parent().addClass('navselected');
+				pushRelStateAndLoad();
+				return false;
+			});
+			//clicking on paging nav
+			if($('#paging').length){
+				$(this).delegate('a', 'click', function(){
+					_link = $(this).attr('href');
+					pushRelStateAndLoad();
+					return false;
+				});
+			}
+			//clicking on a post
+			if($('.post').length){
+				$(this).delegate('a', 'click', function(){
+					_link = $(this).attr('href');
+					pushRelStateAndLoad();
+					return false;
+				});
+			}
+			//back button
+			$(window).bind('popstate', function(){
+				if(chrome_popped){
+		       		_link = location.pathname.split('/')[1];
+		       		console.log("Pop event fired: '"+ _link+"'");
+		 			loadContent(_link);
+	       			var $nav_selected = $('nav ul li');
+	       			$nav_selected.find('a[href$='+ _link +']').parent().addClass('navselected');
+	 			}
+	    	});
+			//load landing page content
+			$('nav li:eq(1)').addClass('navselected');
+			loadContent("/blog");
+		}
+		function pushRelStateAndLoad(){
+			chrome_popped = true;
+			history.pushState(null, null, _link);
+			//_link = base_link + _link;
+			loadContent(_link);
+		}
+		function loadContent(href){
+			$.get(href,function(data){
+				var	new_content = $(data).filter('#content').html(),
+					new_paging = $(data).filter('#paging').html();
+
+				var $old_posts = $('article.fadeIn');
+				if($old_posts.length){
+					var last_to_fade = '.post'+ ($old_posts.length-1);
+					$old_posts.addClass('fadeOut');
+					$old_posts.filter(last_to_fade).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e){
+						postLoadContent();				
+					});
+				}else{
+					postLoadContent();
+				}
+				function postLoadContent(){
+					$content_div.html(new_content);
+					$paging_div.html(new_paging);
+					var $posts = $('article.post');
+					if($posts.length){
+						$posts.addClass('fadeIn').each(function(i){
+							$(this).addClass('post'+(i));
+						});
+						//plus paging contents
+					}else{
+						var $post_title = $('#post-title');
+						if($post_title.length){
+							$post_title.addClass('slideInFromTop').siblings().addClass('fadeIn');
+							eval($(data).filter('script').text());
+						}
+					}				
+				}
+			});
+		}
+	}
+);
